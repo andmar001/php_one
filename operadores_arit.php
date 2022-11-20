ca5269b231626f8feecd101fc67d3da021e9dd52
tony tolver <andrademaresantonio00@gmail.com>
2022-11-20 01:58:09 -0600

    operadores artimeticos

---
+++ operadores_arit.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Operadores aritméticos";
+
+$suma = 2 + 2;
+echo $suma;
+
+$resta = 2 - 2;
+echo $resta;
+
+$multiplicacion = 2 * 2;
+echo $multiplicacion;
+
+$division = 2 / 2;
+echo $division;
+
+$modulo = 2 % 2;
+echo $modulo;
+
+$exponente = 2 ** 2;
+echo $exponente;
+
+
+echo "7200 seconds to hours is: " . (7200 / 3600) . " hours";
+echo "Sobran: " . (7501 % 3600) . " minutes";
