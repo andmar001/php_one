08425ba953a55850299421a4623ff0dee1a212a6
tony tolver <andrademaresantonio00@gmail.com>
2022-11-20 20:58:13 -0600

    operadores relacionales especiales

--- operadores_relacionales.php
+++ operadores_relacionales.php
@@ -16,3 +16,22 @@ var_dump($a === $b); // false
 // diferente
 var_dump($a != $b); // false
 
+var_dump($a !== $b); // false
+
+// less than
+var_dump($a < $c); // true
+
+// less than or equal to
+var_dump($a <= $b); // true
+
+// mayor que
+var_dump($a >= $b); // false
+
+var_dump($a == $b); // true
+var_dump($a === $b); // false
+
+// operador nave espacial  <=>
+// si el de la izquierda es menor que el de la derecha devuelve -1
+echo 2 <=> 1;   // 1 
+echo 1 <=> 2;  //  -1
+echo 1 <=> 1;  // 0
